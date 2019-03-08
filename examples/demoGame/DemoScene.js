a9406daf4c7344d09c363726528140db2c145b6c
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2019-03-07 20:15:59 -0500

    Fixed that text was not drawing on screen:

--- examples/demoGame/DemoScene.js
+++ examples/demoGame/DemoScene.js
@@ -8,12 +8,12 @@ export default class DemoScene extends Scene {
   create() {
     this.rectangle = new Rectangle(this.Game.context, new Point(20, 20), 150, 200);
     this.circle = new Circle(this.Game.context, new Point(100, 100), 20);
-    this.text = new Text(this.Game.context, 200, 200, 'Hello World!');
+    this.text = new Text(this.Game.context, new Point(200, 200), 'Hello World!');
   }
 
   update() {
     // this.rectangle.Width += 1;
-    // this.text.X += 1;
+    // this.text.Point.X += 1;
   }
 
   draw() {
