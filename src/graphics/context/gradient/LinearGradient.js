90a10b2d6f228661b1010192edf9da8603b77f85
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2019-03-07 22:30:51 -0500

    document the linear gradient class

--- src/graphics/context/gradient/LinearGradient.js
+++ src/graphics/context/gradient/LinearGradient.js
@@ -2,12 +2,22 @@ import Gradient from './Gradient';
 
 /** @extends Gradient */
 class LinearGradient extends Gradient {
+  /**
+  * Constructs a new LinearGradient object.
+  *
+  * @param {CanvasRenderingContext2D} context the canvas context.
+  * @param {Point} point1 the starting point of the gradient.
+  * @param {Point} point2 the ending point of the gradient.
+  */
   constructor(context, point1, point2) {
     super(context);
     this.Point1 = point1;
     this.Point2 = point2;
   }
 
+  /**
+  * Builds the linear gradient.
+  */
   BuildGradient() {
     const gradient = this.context.createLinearGradient(
       this.Point1.X, this.Point2.X, this.Point2.X, this.Point2.Y,
