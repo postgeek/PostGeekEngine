31922a941c1b5e58e12bb0366a1813c2092e706c
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2019-03-31 19:37:31 -0400

    removed the watch from webpack.dev.js file

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,12 +3,11 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  //watch: true,
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 3000
-  }
+    port: 3000,
+  },
 });
