da2abe0e41ed37c8ae39b2f37c70f449ae0d8704
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2019-04-02 00:31:52 -0400

    Added the transformation class for manipulating the context

---
+++ src/graphics/context/Transformation.js
@@ -0,0 +1,60 @@
+
+class Transformation {
+  constructor(context) {
+    this.Context = context;
+  }
+
+  /**
+   * The CanvasRenderingContext2D.
+   */
+  get Context() {
+    return this.context;
+  }
+
+  set Context(value) {
+    this.context = value;
+  }
+
+  /**
+   * Begin a new transformation.
+   */
+  BeginTransform() {
+    this.Context.save();
+  }
+
+  /**
+   * Rotates the current context in degrees
+   */
+  Rotate(degree) {
+    this.Context.rotate(degree * Math.PI / 180);
+  }
+
+  /**
+   * Scale the current context.
+   */
+  Scale(x, y) {
+    this.Context.scale(x, y);
+  }
+
+  /**
+   * Translate the current context.
+   */
+  Translate(x, y) {
+    this.Context.translate(x, y);
+  }
+
+  Skew(x, y) {
+    this.Context.transform(1, x, y, 1, 1, 1);
+  }
+
+  Reset() {
+    this.Context.resetTransform();
+  }
+
+  /**
+   * Ends the transformation.
+   */
+  EndTransform() {
+    this.Context.restore();
+  }
+} export default Transformation;
