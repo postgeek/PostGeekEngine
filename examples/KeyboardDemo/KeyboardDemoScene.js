8f1134a17e555d1110c9422141f24fb7fed9a7a4
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2020-04-17 13:37:01 -0400

    Added a A,B,C to the list of printed characters on screen fixes #22

--- examples/KeyboardDemo/KeyboardDemoScene.js
+++ examples/KeyboardDemo/KeyboardDemoScene.js
@@ -1,20 +1,30 @@
 import Scene from 'gameEngine/scene/Scene';
 import TextGraphic from 'renderingEngine/text/TextGraphic';
+import Point from 'core/Point';
 import KeyboardKey from 'inputEngine/KeyboardKey';
 
 export default class KeyboardDemoScene extends Scene {
   create() {
     console.log(KeyboardKey.A);
     this.game.Keyboard.registerKey(KeyboardKey.A);
+    this.game.Keyboard.registerKey(KeyboardKey.B);
+    this.game.Keyboard.registerKey(KeyboardKey.C);
+    this.KeyboardKeyPressed = new TextGraphic(new Point(100, 140), '');
   }
 
   update() {
     if (this.game.Keyboard.keyDownHeld(KeyboardKey.A)) {
-      console.log('A is down');
+      this.KeyboardKeyPressed.text = "A";
+    }
+    if (this.game.Keyboard.keyDownHeld(KeyboardKey.B)) {
+      this.KeyboardKeyPressed.text = "B";
+    }
+    if (this.game.Keyboard.keyDownHeld(KeyboardKey.C)) {
+      this.KeyboardKeyPressed.text = "C";
     }
   }
 
   draw() {
-
+    this.KeyboardKeyPressed.draw();
   }
 }
