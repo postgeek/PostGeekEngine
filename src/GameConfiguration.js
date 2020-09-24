5beaa9cc2a9bbd81096a8f3a204ddf478f6b54cf
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2020-09-23 21:17:09 -0400

    Feature/game unit tests (#78)
    * Added initial test case
    
    * Added npm packages for testing Canvas and Context2D. Removed the console printouts in the PostGeekDebugger. Added two unit tests to the Game.test.js for checking that the canvas width and height are properly set
    
    * Added static modified to the isConfigValid method
    
    * removed the this before calling isConfigValid
    
    * Added a containsKey method to the service locator as well as associated unit tests. Changed the way the Game object is created to facilitate testing. Modified the main.js to reflect the new way of creating the Game object.
    
    * Removed unused import
    
    * Added test to make sure adding scenes was done properly. Modified a test to make sure when start is called the isStarted bool is set to true
    
    * Added unit tests for the Game class.
    
    * Update Game.test.js
    
    Co-authored-by: Edouard Shaar <zarthia@gmail.com>

---
+++ src/GameConfiguration.js
@@ -0,0 +1,43 @@
+import InvalidArguementError from './core/errorHandling/errors/InvalidArguementError';
+
+class GameConfiguration {
+  constructor(canvas, initialScene) {
+    if (!GameConfiguration.isConfigValid({ canvas, initialScene })) {
+      throw new InvalidArguementError(this);
+    }
+    this.canvas = canvas;
+    this.initialScene = initialScene;
+    this.middleware = {};
+  }
+
+  set middleware(value) {
+    this._middleware = value;
+  }
+
+  get middleware() {
+    return this._middleware;
+  }
+
+  set initialScene(value) {
+    this._initialScene = value;
+  }
+
+  get initialScene() {
+    return this._initialScene;
+  }
+
+  set canvas(value) {
+    this._canvas = value;
+  }
+
+  get canvas() {
+    return this._canvas;
+  }
+
+  static isConfigValid(gameConfiguration) {
+    const { canvas, initialScene } = gameConfiguration;
+    return (gameConfiguration !== undefined)
+        && (canvas !== undefined)
+        && (initialScene !== undefined);
+  }
+} export default GameConfiguration;
